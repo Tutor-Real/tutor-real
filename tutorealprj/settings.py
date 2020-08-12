f0b0eec3dd0fc226df5e48c0596544d908075c70
Yewon <kyw20160785@gmail.com>
2020-08-12 21:30:37 +0900

    committ

--- tutorealprj/settings.py
+++ tutorealprj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'h)c!$m13ti+$xgzvrym!2$*95qsf6nud*&mvo5t8q3g5i_=ndp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'tutorealprj.urls'
